ebfc835bba54fff98301f75a36dc05d352ec514d
edvaldotorres <edvaldo.torres@tisaude.com>
2023-03-16 00:35:34 -0300

    :construction: routes and controllers for patient and address

--- app/routes/api.php
+++ app/routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\PatientController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::apiResources([
+    'patients' => PatientController::class,
+]);
