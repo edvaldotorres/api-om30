755b70bcc2be68218dabdf70f0d18f7d31429db6
edvaldotorres <edvaldo.torres@tisaude.com>
2023-03-16 17:42:06 -0300

    :bulb: comment on models

--- app/app/Models/Address.php
+++ app/app/Models/Address.php
@@ -12,7 +12,7 @@ class Address extends Model
     /**
      * The attributes that are mass assignable.
      *
-     * @var array
+     * @var array<int, string>
      */
     protected $fillable = [
         'patient_id',
--- app/app/Models/Patient.php
+++ app/app/Models/Patient.php
@@ -14,7 +14,7 @@ class Patient extends Model
     /**
      * The attributes that are mass assignable.
      *
-     * @var array
+     * @var array<int, string>
      */
     protected $fillable = [
         'image',
