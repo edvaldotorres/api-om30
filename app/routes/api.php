41a4281546a6f9eeb176538c2955c0e9ab11bf17
edvaldotorres <edvaldo.torres@tisaude.com>
2023-03-17 16:30:19 -0300

    :recycle: Refactor: route

--- app/routes/api.php
+++ app/routes/api.php
@@ -16,16 +16,14 @@ use App\Http\Controllers\Api\v1\ZipCodeController;
 |
 */
 
-Route::get('/', function () {
-    return response()->json(['message' => 'Bem-vindo a API da OM30 <3']);
-});
-
 Route::prefix('v1')->group(function () {
+    Route::get('/', function () {
+        return response()->json(['message' => 'Bem-vindo a API da OM30 <3']);
+    });
     Route::prefix('auth')->group(function () {
         Route::post('login', [AuthController::class, 'login']);
         Route::post('register', [AuthController::class, 'register']);
     });
-
     Route::middleware('jwt.api')->group(function () {
         Route::apiResource('patients', PatientController::class)->only([
             'index', 'store', 'show', 'update', 'destroy'
