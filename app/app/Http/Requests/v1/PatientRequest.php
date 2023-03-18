e095d5b1c9291f4fa26952fd5af05a1093592104
edvaldotorres <edvaldo.torres@tisaude.com>
2023-03-18 20:35:25 -0300

    :sparkles: Add validation rule for CNS e & unique validation rule for CNS e CPF

--- app/app/Http/Requests/v1/PatientRequest.php
+++ app/app/Http/Requests/v1/PatientRequest.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Requests\v1;
 
-use App\Rules\v1\CnsValidation1Rule;
-use App\Rules\v1\CnsValidation2Rule;
+use App\Rules\v1\CnsValidationRule;
+use Illuminate\Validation\Rule;
 
 class PatientRequest extends BaseRequest
 {
@@ -15,13 +15,23 @@ class PatientRequest extends BaseRequest
     public function rules()
     {
         return [
-            'image' => 'nullable|string',
+            'image' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
             'name' => 'required|string|min:3|max:255',
-            'mother_name' => 'required|string|min:3|max:255',
+            'mother_name' => 'nullable|string|min:3|max:255',
             'birth_date' => 'required|date_format:d/m/Y',
-            'cpf' => 'required|string|min:11|max:11',
-            'cns' => ['required', 'string', new CnsValidation1Rule(), new CnsValidation2Rule()],
-            'zip_code' => 'required|string|min:8|max:8',
+            'cpf' => [
+                'required',
+                'string',
+                'min:11',
+                'max:11',
+                Rule::unique('patients')->ignore($this->patient),
+            ],
+            'cns' => [
+                'nullable',
+                Rule::unique('patients')->ignore($this->patient),
+                new CnsValidationRule()
+            ],
+            'zip_code' => 'nullable|string|min:8|max:8',
             'street' => 'required|string|min:3|max:255',
             'number' => 'nullable|string',
             'complement' => 'nullable|string',
